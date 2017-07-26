0a10cf94ff53cb5e4a5200d2e850de17ce13dbc4
Al Miller <al.miller@ajmiller.net>
2017-07-26 15:23:11 -0700

    get start/end query working

--- lambdas/get.js
+++ lambdas/get.js
@@ -4,7 +4,6 @@ const doc = require('dynamodb-doc');
 const dynamo = new doc.DynamoDB();
 
 exports.handler = (event, context, callback) => {
-    console.log("AJM: in GET")
     console.log('Received event:', JSON.stringify(event, null, 2));
     const done = (err, res) => callback(null, {
         statusCode: err ? '400' : '200',
@@ -13,5 +12,20 @@ exports.handler = (event, context, callback) => {
             'Content-Type': 'application/json',
         },
     });
-    dynamo.scan({ TableName: "tank2" }, done);
+    let startTime = event.params.querystring.startTime;
+    let endTime = event.params.querystring.endTime;
+    let params = {
+        TableName: "tank2",
+        KeyConditionExpression: "tank = :tk AND #ts BETWEEN :start and :end",
+        ExpressionAttributeNames: {
+            "#ts": "timestamp"
+        },
+        ExpressionAttributeValues: {
+            ":tk": "secondary",
+            ":start": startTime,
+            ":end": endTime
+        }
+    };
+    console.log(params);
+    dynamo.query(params, done);
 };
