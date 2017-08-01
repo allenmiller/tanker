5e58f969355b97d309b67fce7a4c205095a97002
Al Miller <al.miller@ajmiller.net>
2017-08-01 16:19:42 -0700

    responsiveness, and fix length of day

--- ui/tanker/src/App.js
+++ ui/tanker/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { LineChart, Line, XAxis, YAxis, Legend } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Legend, ResponsiveContainer } from 'recharts';
 import './App.css';
 import tanker from './services/TankerService';
 
@@ -17,7 +17,7 @@ class App extends React.Component {
 
 let d =  new Date();
 let now = d.getTime();
-let yesterday = now - 24 * 60 * 1000;
+let yesterday = now - 24 * 60 * 60 * 1000;
 
 let data = tanker.getLevels(yesterday, now);
 data.then((levels) => {
@@ -30,14 +30,15 @@ function RenderApp(points) {
 
   console.log(points);
   ReactDOM.render(
-
-        <LineChart width={400} height={400} data={points}>
+      <ResponsiveContainer minHeight={400} maxHeight={800}>
+        <LineChart data={points}>
           <XAxis dataKey="timestamp"/>
           <YAxis/>
           <Legend/>
 
           <Line dataKey="level" />
-        </LineChart>,
+        </LineChart>
+      </ResponsiveContainer>,
 
       document.getElementById('root')
   )
