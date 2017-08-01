ab1989e863a70cc6024f2f7fa52f85b780fe93c5
Al Miller <al.miller@ajmiller.net>
2017-08-01 12:55:43 -0700

    basic graphing

--- ui/tanker/src/App.js
+++ ui/tanker/src/App.js
@@ -1,36 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { LineChart, Line, XAxis, YAxis, Legend } from 'recharts';
 import './App.css';
 import tanker from './services/TankerService';
 
-class App extends Component {
-
+class App extends React.Component {
   render() {
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Tanker</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-            State is {this.state}
-        </p>
-      </div>
-    );
+        <div>
+        {this.props.children}
+  </div>
+  );
   }
 }
 
 let d =  new Date();
 let now = d.getTime();
 let yesterday = now - 24 * 60 * 1000;
+let levels;
 
 let data = tanker.getLevels(yesterday, now);
 data.then((levels) => {
-    console.log(levels);
+    console.log(levels.Items);
+    let arr = Array.from(levels.Items);
+    console.log(arr);
+    RenderApp(arr);
 });
-console.log(data);
 
+function RenderApp(points) {
 
+  ReactDOM.render(
+      <div className="AJM"> Hello
+        <LineChart width={400} height={400} data={points}>
+          <XAxis dataKey="timestamp"/>
+          <YAxis/>
+          <Legend/>
+          <Line type="monotone" dataKey="level" stroke="#8884d8" />
+        </LineChart>
+      </div>,
+      document.getElementById('root')
+  )
+}
 
 export default App;
--- ui/tanker/src/services/TankerService.js
+++ ui/tanker/src/services/TankerService.js
@@ -1,28 +1,29 @@
 export default {
 
-    getLevels: function (start, end) {
-        let url = "https://3nxzqsgs8k.execute-api.us-east-1.amazonaws.com/test/tanker?"
-        //let url = "https://rmecu0chj5.execute-api.us-east-1.amazonaws.com/prod/tanker?";
-        url += "startTime=" + start;
-        url += "&endTime=" + end;
+  getLevels: function (start, end) {
+    let url = "https://3nxzqsgs8k.execute-api.us-east-1.amazonaws.com/test/tanker?"
+    //let url = "https://rmecu0chj5.execute-api.us-east-1.amazonaws.com/prod/tanker?";
+    url += "startTime=" + start;
+    url += "&endTime=" + end;
 
-        return fetch(url, {
-            method: 'GET',
-        })
-            .then((response) => {
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        throw {
-                            message: "Tanker service error : " + response.status,
-                            responseStatus: {
-                                code: response.status,
-                                message: response.statusText
-                            },
-                            body: response.text()
-                        }
-                    }
-                }
-            );
-    },
+    return fetch(url, {
+      method: 'GET',
+    })
+    .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            // eslint-disable-next-line
+            throw {
+              message: "Tanker service error : " + response.status,
+              responseStatus: {
+                code: response.status,
+                message: response.statusText
+              },
+              body: response.text()
+            }
+          }
+        }
+    );
+  },
 }
