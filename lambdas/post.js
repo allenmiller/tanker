ed60cce2b414fb6a35a9b3b15cb7ad8ee1b8ce9a
Al Miller <al.miller@ajmiller.net>
2017-08-04 22:30:28 -0700

    add rawLevel.  whitespace

--- lambdas/post.js
+++ lambdas/post.js
@@ -5,25 +5,26 @@ const dynamo = new doc.DynamoDB();
 
 exports.handler = (event, context, callback) => {
 
-    // console.log('AJM: Received event:', JSON.stringify(event, null, 2));
-    // console.log('context:', JSON.stringify(context, null, 2));
-    const done = (err, res) => {
-        // console.log("AJM: in done()", err, res);
-        callback(null,{
-        statusCode: err ? '400' : '200',
-        body: err ? err.message : JSON.stringify(res),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-     });
-    };
-    let d = new Date();
-    let record = { 
-        "tank": "secondary",
-        "timestamp": d.getTime(), 
-        "level": parseInt(event.params.querystring.level),
-        "sourceIp": event.context.sourceIp
-    };
-    console.log("writing: ", record);
-    dynamo.putItem({TableName: "tank2", Item: record}, done);
+  // console.log('AJM: Received event:', JSON.stringify(event, null, 2));
+  // console.log('context:', JSON.stringify(context, null, 2));
+  const done = (err, res) => {
+    // console.log("AJM: in done()", err, res);
+    callback(null, {
+      statusCode: err ? '400' : '200',
+      body: err ? err.message : JSON.stringify(res),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+  let d = new Date();
+  let record = {
+    "tank": "secondary",
+    "timestamp": d.getTime(),
+    "rawLevel": parseFloat(event.params.querystirng.rawLevel),
+    "level": parseInt(event.params.querystring.level),
+    "sourceIp": event.context.sourceIp
+  };
+  console.log("writing: ", record);
+  dynamo.putItem({TableName: "tank2", Item: record}, done);
 };
