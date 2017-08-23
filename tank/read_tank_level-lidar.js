c8d68c7201c72de5d3ed2d905eb982c3a0836288
Al Miller <al.miller@ajmiller.net>
2017-08-22 23:07:13 -0700

    implement reading from lidar sensor

--- tank/read_tank_level-lidar.js
+++ tank/read_tank_level-lidar.js
@@ -1,53 +1,43 @@
-let ads1x15 = require('node-ads1x15');
+let PythonShell = require('python-shell');
+let pyshell = new PythonShell('test.py', {scriptPath: '/home/pi/git/github.com/Sanderi44/Lidar-Lite/python/'});
 let request = require('request');
+let sensor = 'LL-905-PIN-01';
 
-let chip = 1; //0 for ads1015, 1 for ads1115
-let adc = new ads1x15(chip);
-
-let channel = 0;
-let samplesPerSecond = '250'; // see index.js for allowed values for your chip
-let progGainAmp = '2048'; // see index.js for allowed values for your chip
-
-let reading;
-let factor = (5.0 * 25.4) / 512;  // (Vcc * 25.4 mm/inch) / (512 mv/inch)
 let date = new Date();
-
 let postUrl = 'https://rmecu0chj5.execute-api.us-east-1.amazonaws.com/prod/tanker';
 
 function post_result(record) {
 
-  console.log("Posting ", record);
-  request.post({
-      url: postUrl,
-      json: true,
-      body: record
-  }, function (error, response, body) {
-    if (error) {
-      console.log("ERROR");
-      console.log(response);
-      console.log(body);
-      throw error;
-    }
+    console.log("Posting ", record);
+    request.post({
+	url: postUrl,
+	json: true,
+	body: record
+    }, function (error, response, body) {
+	if (error) {
+	    console.log("ERROR");
+	    console.log(response);
+	    console.log(body);
+	    throw error;
+	}
 
-  });
+    });
 }
 
-if (!adc.busy) {
-  adc.readADCSingleEnded(channel, progGainAmp, samplesPerSecond,
-      function (err, data) {
-        if (err) {
-          throw err;
-        }
+pyshell.on('message', (distanceStr) => {
+    "use strict";
+    console.log(distanceStr);
+    let distance = parseInt(distanceStr);
+    
+    let record = {};
 
-        reading = parseFloat(data);
+    record.distance_cm = distance;
+    record.time = date.getTime();
+    record.sensor = sensor;
+    
+    post_result(record);
 
-        let distance = reading * factor;
-        let record = {};
+});
 
-        record.reading_mV = reading;
-        record.distance_cm = distance;
-        record.time = date.getTime();
+pyshell.end();
 
-        post_result(record);
-      });
-}
