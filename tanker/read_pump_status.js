baed393a8952ffffb1ad008bb973f9783b620078
Al Miller <al.miller@ajmiller.net>
2017-09-16 23:05:52 -0700

    work on gpio pin reading

--- tanker/read_pump_status.js
+++ tanker/read_pump_status.js
@@ -1,8 +1,42 @@
-var gpio = require('rpi-gpio');
+const {exec} = require('child_process');
 
-gpio.on('change', function(channel, value) {
-    console.log('Channel ' + channel + ' value is now ' + value);
-});
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-console.log("calling setup");
-gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
+async function readPin() {
+  let priorState;
+  console.log("in readPin()");
+  while (true) {
+    console.log("true");
+    exec('gpio read 25', (err, stdout, stderr) => {
+      if (err) {
+        console.log(err);
+        // node couldn't execute the command
+        return;
+      }
+
+      // the *entire* stdout and stderr (buffered)
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+
+      let value = parseInt(stdout);
+      console.log(value);
+      if (!priorState) {
+        // if priorState is undefined, initialize it to the first value;
+        console.log("priorState is undefined");
+        priorState = value;
+      }
+
+      if (priorState !== value) {
+        console.log("State change!");
+        priorState = value;
+      }
+
+    });
+    await sleep(2000);
+  }
+
+}
+
+readPin();
