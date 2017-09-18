ca5a6f80e4ea95a86ead8f5cd1030d0e917d38dc
Al Miller <al.miller@ajmiller.net>
2017-09-17 18:36:45 -0700

    node doesn't support import/export yet

--- tanker/read_tank_level-lidar.js
+++ tanker/read_tank_level-lidar.js
@@ -1,9 +1,6 @@
-import Utils from './utils';
-
-
 let PythonShell = require('python-shell');
 let pyshell = new PythonShell('test.py',
-    {scriptPath: '/home/pi/git/github.com/Sanderi44/Lidar-Lite/python/'});
+  {scriptPath: '/home/pi/git/github.com/Sanderi44/Lidar-Lite/python/'});
 
 const date = new Date();
 const sensor = 'LL-905-PIN-01';
@@ -12,6 +9,25 @@ const SENSOR_LEVEL = 40;  // cm below ground level
 process.argv.forEach(function (val, index, array) {
   console.log(index + ': ' + val);
 });
+
+post_result = (record) => {
+
+  const postUrl = 'https://rmecu0chj5.execute-api.us-east-1.amazonaws.com/prod/tanker';
+  console.log("Posting ", record);
+  request.post({
+    url: postUrl,
+    json: true,
+    body: record
+  }, function (error, response, body) {
+    if (error) {
+      console.log("ERROR");
+      console.log(response);
+      console.log(body);
+      throw error;
+    }
+
+  });
+};
 
 pyshell.on('message', (distanceStr) => {
   "use strict";
@@ -25,7 +41,7 @@ pyshell.on('message', (distanceStr) => {
   record.distance_cm = distance;
   record.time = date.getTime();
 
-  Utils.post_result(record);
+  post_result(record);
 
 });
 
