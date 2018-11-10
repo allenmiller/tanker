f7c107da58158ed1e46136251399e6ad24dde9dd
Al Miller <al.miller@ajmiller.net>
2018-11-09 20:19:13 -0800

    fix paths

--- tanker/run.sh
+++ tanker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while [ 1 ] ; do
-    node read_tank_level-lidar.js 40
+    node /home/pi/git/github.com/allenmiller/tanker-backend/tanker/read_tank_level-lidar.js 40
     sleep 300
 done
