b2eff9586f6711fe9e189b8ea84508eff5275c64
Al Miller <al.miller@ajmiller.net>
2020-06-15 13:27:43 -0700

    Node.js program for reading distance from MaxBotix I2C

--- tanker/read_maxbotix_i2c.js
+++ tanker/read_maxbotix_i2c.js
@@ -2,28 +2,33 @@
 
 const i2c = require('i2c-bus');
 
-const MAXBOTIX_BUS_ADDR = 0x70 // 224
-const MAXBOTIX_READ_ADDR  = 0x70 // 225
-const MEASURE_RANGE_COMMAND  = 0x51
+const I2C_BUS                  = 1
+const MAXBOTIX_BUS_ADDR        = 0x70
+const MAXBOTIX_WAIT_TIME_MS    = 100
+const MEASURE_RANGE_COMMAND    = 0x51
 const READ_MEASUREMENT_ADDRESS = 0xE1
 
+function swapBytes(word) {
+    return ((word >> 8) & 0xff) | ((word & 0xff) << 8);
+}
+
+async function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function getDistance() {
-    const i2c1 = i2c.openSync(1);
-    let bus = i2c1.writeByteSync(MAXBOTIX_BUS_ADDR, 0, MEASURE_RANGE_COMMAND)
-    let start = Date.now()
-    console.log("sleeping: ", start)
-    await new Promise(resolve => setTimeout(resolve, 100));
-    let end = Date.now()
-    console.log("awake: ", end - start)
-    const rawData = i2c1.readWordSync(MAXBOTIX_BUS_ADDR, READ_MEASUREMENT_ADDRESS);
-    console.log(rawData.toString(16));
-    swapped = ((rawData >> 8) & 0xff) | ((rawData & 0xff) << 8);
-    console.log( swapped.toString(16));
-    console.log(swapped, " cm")
-    i2c1.closeSync();
-    return swapped
+    const i2cBus = i2c.openSync(I2C_BUS);
+    i2cBus.writeByteSync(MAXBOTIX_BUS_ADDR, 0, MEASURE_RANGE_COMMAND)
+    await delay(MAXBOTIX_WAIT_TIME_MS)
+    const rawData = i2cBus.readWordSync(MAXBOTIX_BUS_ADDR, READ_MEASUREMENT_ADDRESS);
+    i2cBus.closeSync();
+    return swapBytes(rawData)
+}
+
+function postDistance(distance) {
+    console.log(distance)
 }
 
 getDistance()
-    .then(distance => { console.log(distance, "cm") })
+    .then(distance => { postDistance(distance) })
     .catch(err => { console.log(err) })
