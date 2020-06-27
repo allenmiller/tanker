9883df80fd7b03206d253e41e5937e8800bcc676
Allen Miller <al.miller@ajmiller.net>
2020-06-26 22:40:56 -0700

    add pump status pins

--- tanker/read_maxbotix_i2c.js
+++ tanker/read_maxbotix_i2c.js
@@ -1,5 +1,7 @@
 //i2cset -y 1 0x70 0x51 && sleep 1 && i2cget -y 1 0x70 0xE1 w
+console.log("Starting")
 
+const Gpio = require('onoff').Gpio;
 const i2c = require('i2c-bus')
 const request = require('request');
 
@@ -9,43 +11,50 @@ const MAXBOTIX_WAIT_TIME_MS    = 100
 const MEASURE_RANGE_COMMAND    = 0x51
 const READ_MEASUREMENT_ADDRESS = 0xE1
 const SENSOR_LEVEL             = 33;  // cm below ground level
-const SENSOR_NAME              = "MAXBOTIX_7040_100"
+const SENSOR_NAME              = "MB_7040_100"
 const SENSOR_OFFSET            = 0;   // constant error in sensor position.
 
 const POST_URL = 'https://rmecu0chj5.execute-api.us-east-1.amazonaws.com/prod/tanker'
 
-const date = new Date()
-
 function swapBytes(word) {
-    return ((word >> 8) & 0xff) | ((word & 0xff) << 8);
+    return ((word & 0xff00) >> 8) | ((word & 0xff) << 8);
 }
 
 async function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function getDistance() {
-    const i2cBus = i2c.openSync(I2C_BUS);
+async function getDistance(i2cBus) {
     i2cBus.writeByteSync(MAXBOTIX_BUS_ADDR, 0, MEASURE_RANGE_COMMAND)
     await delay(MAXBOTIX_WAIT_TIME_MS)
     const rawData = i2cBus.readWordSync(MAXBOTIX_BUS_ADDR, READ_MEASUREMENT_ADDRESS);
-    i2cBus.closeSync();
     return swapBytes(rawData)
 }
 
-function buildRecord(distance) {
+async function driveTankLevelLoop(ms) {
+    const i2cBus = i2c.openSync(I2C_BUS);
+    while (true) {
+	console.log("getting a point")
+	getDistance(i2cBus)
+	    .then(distance => { postResult(distance) })
+	    .catch(err => { console.log(err) })
+	await delay(ms)
+    }
+    i2cBus.closeSync();  // will never hit this, need graceful shutdown
+}
+
+function buildLevelRecord(distance) {
     let record = {};
     record.sensor = SENSOR_NAME;
     record.sensorLevel = SENSOR_LEVEL;
     record.distance_cm = distance + SENSOR_OFFSET;
-    record.time = date.getTime();
+    record.time = (new Date()).getTime();
     return record
 }
 
 function postResult(distance) {
-    record = buildRecord(distance)
+    record = buildLevelRecord(distance)
     console.log("Posting ", record);
-    return
     request.post({
       url: POST_URL,
       json: true,
@@ -61,11 +70,22 @@ function postResult(distance) {
           if (err) throw err;
           console.log("Saved locally")
         });
-        throw error;
+          // throw error;
       }
     });
   };
 
-getDistance()
-    .then(distance => { postResult(distance) })
-    .catch(err => { console.log(err) })
+async function drivePumpSensorLoop(tankName, GpioPin, ms) {
+    const sensor = new Gpio(GpioPin, 'in');
+    while (true) {
+	pumpState = sensor.readSync();
+	console.log("%s: %s", tankName, pumpState)
+	await delay(ms)
+    }
+}
+
+driveTankLevelLoop(15000);
+drivePumpSensorLoop("SEPTIC", 4, 5000)
+drivePumpSensorLoop("SAND", 17, 5000)
+
+console.log("End")
